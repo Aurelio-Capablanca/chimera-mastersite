28cf3f5f55261075111cd5d80df183a0c4ab1cf5
Aurelio-Capablanca <aurelio.iempereur@gmail.com>
2026-03-27 23:39:07 -0600

    organizing
    I'm possing the Rules, not the template, so you're not JS, thankfully!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "ssr")]
 mod backend;
-mod frontend;
+
+pub mod frontend;
 
 #[cfg(feature = "ssr")]
 #[tokio::main]
@@ -10,7 +11,7 @@ async fn main() {
     use leptos::logging::log;
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use chimera_mastersite::app::*;
+    use crate::frontend::view::app::*;
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
